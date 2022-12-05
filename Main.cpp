1b57db549df5dfd06b708a5640d08e55f06e57e8
AvtoBBus <113329450+AvtoBBus@users.noreply.github.com>
2022-12-05 18:47:01 +0400

    Some fix in Main.cpp
    Some fix in Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <conio.h>
 #include <iostream>
-#include <string>
+#include <complex>
+#include <string.h>
 #include "Errors.h"
 #include "Matrix.h"
 
@@ -13,8 +14,28 @@ int get_key() {
 	return key;
 }
 
-Matrix result_of_operation(Matrix first_obj, Matrix second_obj, const char* operation) {
-	Matrix help_obj(0, 0, 0);
+int check_and_input()
+{
+	char NUM[] = { '0','1','2','3','4','5','6','7','8','9' };
+	int number = 0;
+	while (number <= 0)
+	{
+		while (!(cin >> number) || (cin.peek() != '\n'))
+		{
+			cin.clear();
+			while (cin.get() != '\n');
+			cout << "\nIncorrect value...\n";
+		}
+		if (number <= 0) cout << "\nOnly > 0\n";
+
+	}
+
+	return number;
+}
+
+template <class T>
+Matrix<T> result_of_operation(Matrix<T> first_obj, Matrix<T> second_obj, const char* operation) {
+	Matrix<T> help_obj(0, 0, (T)0);
 	help_obj = first_obj;
 	if (strcmp("add", operation) == 0) {
 		if (first_obj.get_size_x() == second_obj.get_size_x() && first_obj.get_size_y() == second_obj.get_size_y()) return help_obj + second_obj;
@@ -28,68 +49,45 @@ Matrix result_of_operation(Matrix first_obj, Matrix second_obj, const char* oper
 		return help_obj * second_obj;
 	}
 	else if (strcmp("mul_scalar", operation) == 0) {
-		int scalar = 0, choose = 0;
-		cout << "Chose matrix (1 - FIRST, 2 - SECOND): ";
-		cin >> choose;
+		T scalar = 0;
+		int choose = 0;
+		do {
+			cout << "Chose matrix (1 - FIRST, 2 - SECOND): ";
+			choose = check_and_input();
+		} while (choose < 1 || choose > 2);
 		cout << "Input scalar: ";
-		cin >> scalar;
+		scalar = check_and_input();
 		if (choose == 2) help_obj = second_obj;
 		return help_obj * scalar;
 	}
 	else if (strcmp("div_scalar", operation) == 0) {
-		int scalar = 0, choose = 0;
-		cout << "Chose matrix (1 - FIRST, 2 - SECOND): ";
-		cin >> choose;
+		T scalar = 0;
+		int choose = 0;
+		do {
+			cout << "Chose matrix (1 - FIRST, 2 - SECOND): ";
+			choose = check_and_input();
+		} while (choose < 1 || choose > 2);
 		cout << "Input scalar: ";
-		cin >> scalar;
+		scalar = check_and_input();
 		if (choose == 2) help_obj = second_obj;
-		return help_obj / double(scalar);
+		return help_obj / T(scalar);
 	}
 	else if (strcmp("inverted", operation) == 0) {
 		int choose = 0;
 		do {
 			cout << "Chose matrix (1 - FIRST, 2 - SECOND): ";
-			cin >> choose;
+			choose = check_and_input();
 		} while (choose < 1 || choose > 2);
 		if (choose == 2) help_obj = second_obj;
 		return help_obj.search_invers_matrix();
 	}
+	return help_obj;
 }
 
-int main() {
+template <class T>
+void menu_interaction(Matrix<T> first_obj, Matrix<T> second_obj) {
+	T input_value;
 	int x_size_input = 0, y_size_input = 0, choose = 0;
-	double input_value = 0;
-
-	cout << "INPUT INFO ABOUT 1-ST MATRIX:" << endl;
-	do {
-		cout << "Number of colums: ";
-		cin >> y_size_input;
-	} while (y_size_input <= 0);
-	do {
-		cout << "Number of lines: ";
-		cin >> x_size_input;
-	} while (y_size_input <= 0);
-
-	cout << endl << "INPUT VALUE, WHICH FILL 1-ST MATRIX" << endl;
-	cin >> input_value;
-
-	Matrix first_obj(x_size_input, y_size_input, input_value);
-
-	cout << endl << "INPUT INFO ABOUT 2-D MATRIX:" << endl;
-	do {
-		cout << "Number of colums: ";
-		cin >> y_size_input;
-	} while (y_size_input <= 0);
-	do {
-		cout << "Number of lines: ";
-		cin >> x_size_input;
-	} while (y_size_input <= 0);
-
-	cout << endl << "INPUT VALUE, WHICH FILL 2-D MATRIX" << endl;
-	cin >> input_value;
-
-	Matrix second_obj(x_size_input, y_size_input, input_value);
-
 	while (true) {
 		system("cls");
 		cout << "FIRST MATRIX:" << endl;
@@ -187,7 +185,7 @@ int main() {
 			}
 			catch (Error& er) { er.print_message(); }
 			break;
-		case 122:
+		case 122: //z
 			try {
 				system("cls");
 				cout << endl << result_of_operation(first_obj, second_obj, "inverted");
@@ -195,11 +193,117 @@ int main() {
 			catch (Error& er) { er.print_message(); }
 			break;
 		case 27: //esc
-			return 0;
+			return;
 		}
 		system("pause");
 
 	}
+}
 
+template <class T>
+int second_menu() {
+	int x_size_input, y_size_input, choose;
+	T input_value = 0;
+	system("cls");
+	cout << "INPUT INFO ABOUT 1-ST MATRIX:" << endl;
+	cout << "Number of colums: ";
+	y_size_input = check_and_input();
+	cout << "Number of lines: ";
+	x_size_input = check_and_input();
+
+	cout << endl << "INPUT VALUE, WHICH FILL 1-ST MATRIX" << endl;
+	cin >> input_value;
+
+	Matrix<T> first_obj(x_size_input, y_size_input, input_value);
+
+	cout << endl << "INPUT INFO ABOUT 2-D MATRIX:" << endl;
+	do {
+		cout << "Number of colums: ";
+		cin >> y_size_input;
+	} while (y_size_input <= 0);
+	do {
+		cout << "Number of lines: ";
+		cin >> x_size_input;
+	} while (y_size_input <= 0);
+
+	cout << endl << "INPUT VALUE, WHICH FILL 2-D MATRIX" << endl;
+	cin >> input_value;
+
+	Matrix<T> second_obj(x_size_input, y_size_input, input_value);
+	menu_interaction(first_obj, second_obj);
 	return 0;
+}
+
+template <class T>
+int second_menu_complex() {
+	system("cls");
+	int x_size_input = 0, y_size_input = 0;
+	T input_value_integer, input_value_imaginary;
+	cout << "INPUT INFO ABOUT 1-ST MATRIX:" << endl;
+	cout << "Number of colums: ";
+	y_size_input = check_and_input();
+	cout << "Number of lines: ";
+	x_size_input = check_and_input();
+
+	char input[256];
+	cout << endl << "INPUT VALUE, WHICH FILL 1-ST MATRIX" << endl;
+	cout << endl << "INPUT INTEGER PART" << endl;
+	cin >> input;
+	input_value_integer = stoi(input);
+	cout << endl << "INPUT IMAGINARY PART" << endl;
+	cin >> input;
+	input_value_imaginary = stoi(input);
+
+	Matrix<T> first_obj(x_size_input, y_size_input, (input_value_integer, input_value_imaginary));
+
+	cout << "INPUT INFO ABOUT 2-D MATRIX:" << endl;
+	cout << "Number of colums: ";
+	y_size_input = check_and_input();
+	cout << "Number of lines: ";
+	x_size_input = check_and_input();
+
+	cout << endl << "INPUT VALUE, WHICH FILL 2-D MATRIX" << endl;
+	cout << endl << "INPUT INTEGER PART" << endl;
+	cin >> input;
+	input_value_integer = stoi(input);
+	cout << endl << "INPUT IMAGINARY PART" << endl;
+	cin >> input;
+	input_value_imaginary = stoi(input);
+
+	Matrix<T> second_obj(x_size_input, y_size_input, (input_value_integer, input_value_imaginary));
+	menu_interaction(first_obj, second_obj);
+	return 0;
+}
+
+int main() {
+	do {
+		system("cls");
+		cout << "Press [Q] to work with int" << endl;
+		cout << "Press [W] to work with float" << endl;
+		cout << "Press [E] to work with double" << endl;
+		cout << "Press [A] to work with complex float" << endl;
+		cout << "Press [S] to work with complex double" << endl;
+		cout << "Press [Esc] to exit the program" << endl;
+		int key = get_key();
+		switch (key) {
+		case 113:
+			if (!second_menu<int>()) return 0;
+			break;
+		case 119:
+			if (!second_menu<float>()) return 0;
+			break;
+		case 101:
+			if (!second_menu<double>()) return 0;
+			break;
+		case 97:
+			if (!second_menu_complex<std::complex<float>>()) return 0;
+			break;
+		case 115:
+			if (!second_menu_complex<std::complex<double>>()) return 0;
+			break;
+		case 27:
+			return 0;
+			break;
+		}
+	} while (true);
 }
